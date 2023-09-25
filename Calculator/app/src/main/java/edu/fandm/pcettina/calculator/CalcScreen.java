3ccf03138b10b4c41b52c1d9dceb73d672dcd379
RatMan24 <98135985+RatMan24@users.noreply.github.com>
2023-09-25 11:59:46 -0400

    Android Studio Repo

---
+++ Calculator/app/src/main/java/edu/fandm/pcettina/calculator/CalcScreen.java
@@ -0,0 +1,14 @@
+package edu.fandm.pcettina.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalcScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc_screen);
+    }
+}
